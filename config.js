e580e0f38ee69157e74292c917ffc91a91ee57bf
erikahentsch <erikakhentsch@gmail.com>
2021-07-30 12:07:25 -0400

    added ns data

---
+++ config.js
@@ -0,0 +1,20 @@
+var CONFIG = {
+    elections: [
+        {
+            prov: 'nb', 
+            year: 2020
+        },
+        {
+            prov: 'sk', 
+            year: 2020
+        },
+        {
+            prov: 'bc', 
+            year: 2020
+        },
+        {
+            prov: 'ns', 
+            year: 2021
+        },
+    ]
+}
